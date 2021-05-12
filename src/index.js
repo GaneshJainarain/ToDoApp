8bce20bf0b29ccf20202966219aa1d5f0a5d4e0f
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-05-12 18:48:27 -0400

    creating the graphql schema, typedefs

--- src/index.js
+++ src/index.js
@@ -7,45 +7,22 @@ const {DB_URI, DB_NAME} = process.env;
 dotenv.config();
 
 
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-  
   
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = gql`
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
+  
+    type User {
+        id: ID!
+        name: String!
+        email!
+        avatar!
+    }
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
 `;
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-    Query: {
-      books: () => books,
-    },
+    
   };
-
 
 const start = async () => {
 
@@ -53,6 +30,10 @@ const start = async () => {
     const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     const db = client.db(DB_NAME);
+
+    const context = {
+        db,
+    }
 
 }
     
